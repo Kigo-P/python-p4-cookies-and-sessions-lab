d3b12e35c20b172058907f010de8b4f9acdd6b73
Kigo-P <peterkigo33@gmail.com>
2024-10-11 13:22:41 +0300

    completed lab

--- server/app.py
+++ server/app.py
@@ -22,13 +22,35 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
+    #  getting all the articles from the server
+    article = [article.to_dict() for article in Article.query.all()]
+    # creting and returning a response
+    response = make_response(article, 200)
+    return response
 
-    pass
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    #  setting the session["page_views"] to 0 if the user has never viewed an article
+    session["page_views"] = 0 if not session.get("page_views") else session.get("page_views")
+    # incrementing the session["page_views"] by 1
+    session["page_views"] +=1
 
-    pass
+    # creating an if else ststement to check whether the user had viewed more than 3 pages
+    if session["page_views"] <=3:
+        # show the article in accordance to the  id
+        article = Article.query.filter(Article.id == id).first()
+        #  making the article to a dictionary 
+        article_dict = article.to_dict()
+        # creating and returning a response
+        response = make_response(article_dict, 200)
+        return response
+    else:
+        # create and return a response with a response body
+        response_body = {'message': 'Maximum pageview limit reached'}
+        response = make_response(response_body, 401)
+        return response
+
 
 if __name__ == '__main__':
     app.run(port=5555)
